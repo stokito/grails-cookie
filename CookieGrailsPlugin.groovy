f8973693670edc1138b166653afc1c2faae3e2a2
Sergey Ponomarev <stokito@gmail.com>
2013-01-02 11:42:42 +0200

    Config update

--- CookieGrailsPlugin.groovy
+++ CookieGrailsPlugin.groovy
@@ -1,45 +1,45 @@
 class CookieGrailsPlugin {
-    // the plugin version
-    def version = "0.4"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1 > *"
-    // the other plugins this plugin depends on
+    /** the plugin version */
+    def version = '0.4'
+    /** the version or versions of Grails the plugin is designed for */
+    def grailsVersion = '2.1 > *'
+    /** the other plugins this plugin depends on */
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
+    /** resources that are excluded from plugin packaging */
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+        'grails-app/views/error.gsp'
     ]
 
     // TODO Fill in these fields
-    def author = "Dale Wiggins"
-    def authorEmail = "dale@dalew.com"
-    def title = "Cookie Plugin"
+    def author = 'Dale Wiggins'
+    def authorEmail = 'dale@dalew.com'
+    def title = 'Cookie Plugin'
     def description = '''\\
 Makes dealing with cookies easy.  Provides an injectable service and tag to easily get, set, and delete cookies with one line
 '''
 	def cookieService = new com.dalew.CookieService()
-	
+
 	def observe = ['controllers']
 
-    // URL to the plugin's documentation
-    def documentation = "https://github.com/dalew75/grails-cookie"
+    /** URL to the plugin's documentation */
+    def documentation = 'https://github.com/dalew75/grails-cookie'
 
     // Extra (optional) plugin metadata
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    /** License: one of 'APACHE', 'GPL2', 'GPL3' */
 	def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    /** Any additional developers beyond the author specified above. */
+    def developers = [ [ name: 'Sergey Ponomarev', email: 'stokito@gmail.com' ]]
 
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    /** Location of the plugin's issue tracker. */
+    def issueManagement = [ system: 'github', url: 'https://github.com/dalew75/grails-cookie/issues' ]
 
-    // Online location of the plugin's browseable source code.
-	def scm = [ url: "https://github.com/dalew75/grails-cookie" ]
+    /** Online location of the plugin's browseable source code. */
+	def scm = [ url: 'https://github.com/dalew75/grails-cookie' ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
@@ -69,7 +69,7 @@ Makes dealing with cookies easy.  Provides an injectable service and tag to easi
     def onShutdown = { event ->
         // TODO Implement code that is executed when the application shuts down (optional)
     }
-	
+
 	void extendReqResp() {
 		javax.servlet.http.HttpServletRequest.metaClass.getCookie = { String name ->
 			return cookieService.get(name)
@@ -81,5 +81,5 @@ Makes dealing with cookies easy.  Provides an injectable service and tag to easi
 			return cookieService.delete(name)
 		}
 	}
-	
+
 }
