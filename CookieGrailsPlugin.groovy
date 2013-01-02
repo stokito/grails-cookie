94dbd2970bb054c1604d4a5743d6d506d1310ff4
Sergey Ponomarev <stokito@gmail.com>
2013-01-02 15:46:20 +0200

    Reformating

--- CookieGrailsPlugin.groovy
+++ CookieGrailsPlugin.groovy
@@ -10,7 +10,6 @@ class CookieGrailsPlugin {
             'grails-app/views/error.gsp'
     ]
 
-    // TODO Fill in these fields
     def author = 'Dale Wiggins'
     def authorEmail = 'dale@dalew.com'
     def title = 'Cookie Plugin'
@@ -24,13 +23,8 @@ Makes dealing with cookies easy.  Provides an injectable service and tag to easi
     /** URL to the plugin's documentation */
     def documentation = 'https://github.com/dalew75/grails-cookie'
 
-    // Extra (optional) plugin metadata
-
     /** License: one of 'APACHE', 'GPL2', 'GPL3' */
     def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
 
     /** Any additional developers beyond the author specified above. */
     def developers = [[name: 'Sergey Ponomarev', email: 'stokito@gmail.com']]
@@ -41,33 +35,8 @@ Makes dealing with cookies easy.  Provides an injectable service and tag to easi
     /** Online location of the plugin's browseable source code. */
     def scm = [url: 'https://github.com/dalew75/grails-cookie']
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        extendReqResp()
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
     def onChange = { event ->
         extendReqResp()
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 
     void extendReqResp() {
