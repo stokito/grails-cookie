8b66aeac349223906d4d719abca0d687bed17a24
Sergey Ponomarev <stokito@gmail.com>
2013-01-02 13:35:09 +0200

    Updated version to 0.5

--- CookieGrailsPlugin.groovy
+++ CookieGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class CookieGrailsPlugin {
     /** the plugin version */
-    def version = '0.4'
+    def version = '0.5'
     /** the version or versions of Grails the plugin is designed for */
     def grailsVersion = '2.1 > *'
     /** the other plugins this plugin depends on */
