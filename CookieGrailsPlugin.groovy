7eb7393cefeba48a045c1dff350b6cffaad71902
BeaVe <donbeave@gmail.com>
2014-06-08 13:54:25 +0800

    Some cleans.

--- CookieGrailsPlugin.groovy
+++ CookieGrailsPlugin.groovy
@@ -1,15 +1,30 @@
+/*
+ * Copyright 2012 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author <a href='mailto:dale@dalew.com'>Dale Wiggins</a>
+ * @author <a href='mailto:stokito@gmail.com'>Sergey Ponomarev</a>
+ */
 class CookieGrailsPlugin {
-    /** the plugin version */
+
     def version = '0.51'
-    /** the version or versions of Grails the plugin is designed for */
     def grailsVersion = '2.1 > *'
-    /** the other plugins this plugin depends on */
-    def dependsOn = [:]
-    /** resources that are excluded from plugin packaging */
     def pluginExcludes = [
             'grails-app/views/error.gsp'
     ]
-
     def author = 'Dale Wiggins'
     def authorEmail = 'dale@dalew.com'
     def title = 'Cookie Plugin'
@@ -19,25 +34,22 @@ class CookieGrailsPlugin {
 
     def observe = ['controllers']
 
-    /** URL to the plugin's documentation */
     def documentation = 'https://github.com/dalew75/grails-cookie'
 
-    /** License: one of 'APACHE', 'GPL2', 'GPL3' */
     def license = 'APACHE'
 
-    /** Any additional developers beyond the author specified above. */
-    def developers = [[name: 'Sergey Ponomarev', email: 'stokito@gmail.com']]
+    def developers = [
+            [name: 'Dale Wiggins', email: 'dale@dalew.com'],
+            [name: 'Sergey Ponomarev', email: 'stokito@gmail.com']
+    ]
 
-    /** Location of the plugin's issue tracker. */
-    def issueManagement = [system: 'github', url: 'https://github.com/dalew75/grails-cookie/issues']
-
-    /** Online location of the plugin's browseable source code. */
+    def issueManagement = [system: 'GITHUB', url: 'https://github.com/dalew75/grails-cookie/issues']
     def scm = [url: 'https://github.com/dalew75/grails-cookie']
 
-	def doWithDynamicMethods = { applicationContext ->
-		extendReqResp()
+    def doWithDynamicMethods = { applicationContext ->
+        extendReqResp()
     }
-	
+
     def onChange = { event ->
         extendReqResp()
     }
--- grails-app/services/com/dalew/CookieService.groovy
+++ grails-app/services/com/dalew/CookieService.groovy
@@ -1,8 +1,28 @@
+/*
+ * Copyright 2012 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.dalew
 
-import javax.servlet.http.Cookie
 import org.codehaus.groovy.grails.web.util.WebUtils
 
+import javax.servlet.http.Cookie
+
+/**
+ * @author <a href='mailto:dale@dalew.com'>Dale Wiggins</a>
+ * @author <a href='mailto:stokito@gmail.com'>Sergey Ponomarev</a>
+ */
 class CookieService {
 
     /** Default cookie age is 30 days */
