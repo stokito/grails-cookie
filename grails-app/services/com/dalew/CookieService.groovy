d30dced3d896517ac1a1ac8d6522028950def930
Sergey Ponomarev <stokito@gmail.com>
2014-11-20 19:45:12 +0200

    #22 Support of cookie attributes

--- grails-app/services/com/dalew/CookieService.groovy
+++ grails-app/services/com/dalew/CookieService.groovy
@@ -67,12 +67,17 @@ class CookieService {
      * @param name Cookie name. Can't be blank or null and is case-sensitive
      * @param value Cookie value. Can be blank but not null.
      * @param maxAge Age to store cookie in seconds. Optional
+     * @param path A path to which the client should return the cookie. The cookie is visible to all the pages in the directory. For example, <i>/catalog</i>, which makes the cookie visible to all directories on the server under <i>/catalog</i>. See RFC 2109
+     * @param domain Domain name by RFC 2109. It begins with a dot (.example.com) and means that the cookie is visible to servers in a specified DNS zone (for example, www.example.com, but not a.b.example.com).
+     *               By default, cookies are only returned to the server that sent them.
+     * @param secure Indicates to the browser whether the cookie should only be sent using a secure protocol, such as HTTPS or SSL.
+     * @param httpOnly HttpOnly cookies are not supposed to be exposed to client-side JavaScript code, and may therefore help mitigate certain kinds of cross-site scripting attacks.
      */
-    void setCookie(String name, String value, Integer maxAge = null) {
+    void setCookie(String name, String value, Integer maxAge = null, String path = '/', String domain = null, boolean secure = false, boolean httpOnly = false) {
         assert name
         assert value != null
         maxAge = maxAge ?: defaultCookieAge
-        Cookie cookie = createCookie(name, value, maxAge)
+        Cookie cookie = createCookie(name, value, maxAge, path, domain, secure, httpOnly)
         setCookie(cookie)
     }
 
@@ -97,10 +102,15 @@ class CookieService {
         writeCookieToResponse(cookie)
     }
 
-    private Cookie createCookie(String name, String value, int maxAge) {
+    private Cookie createCookie(String name, String value, int maxAge, String path = '/', String domain = null, boolean secure = false, boolean httpOnly = false) {
         Cookie cookie = new Cookie(name, value)
-        cookie.path = '/'
+        cookie.path = path
         cookie.maxAge = maxAge
+        if (domain) {
+            cookie.domain = domain
+        }
+        cookie.secure = secure
+        cookie.httpOnly = httpOnly
         cookie.version = 1
         return cookie
     }
--- test/unit/com/dalew/CookieServiceSpec.groovy
+++ test/unit/com/dalew/CookieServiceSpec.groovy
@@ -7,6 +7,7 @@ import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.mock.web.MockServletContext
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import javax.servlet.http.Cookie
 
@@ -28,39 +29,48 @@ class CookieServiceSpec extends Specification {
         given:
         def cookie = new Cookie('some_cookie_name', 'cookie_value')
         request.cookies = [cookie]
-        expect: service.findCookie('some_cookie_name') == cookie
+        expect:
+        service.findCookie('some_cookie_name') == cookie
     }
 
     def "getCookie() return cookie value"() {
-        given: request.cookies = [new Cookie('some_cookie_name', 'cookie_value')]
-        expect: service.getCookie('some_cookie_name') == 'cookie_value'
+        given:
+        request.cookies = [new Cookie('some_cookie_name', 'cookie_value')]
+        expect:
+        service.getCookie('some_cookie_name') == 'cookie_value'
     }
 
     def "getCookie() is case-sensitive"() {
-        given: request.cookies = [new Cookie('some_cookie_name', 'cookie_value')]
-        expect: service.getCookie('SoMe_CoOkIe_NaMe') == null
+        given:
+        request.cookies = [new Cookie('some_cookie_name', 'cookie_value')]
+        expect:
+        service.getCookie('SoMe_CoOkIe_NaMe') == null
     }
 
-    void "setCookie() with default age"() {
-        when: service.setCookie('some_cookie_name', 'cookie_value')
-        then:
-        response.cookies[0].name == 'some_cookie_name'
-        response.cookies[0].value == 'cookie_value'
-        response.cookies[0].maxAge == 2592000
+    @Unroll
+    void "setCookie() #name #value #maxAge #path #domain #secure #httpOnly"() {
+        given:
+        service.setCookie(args)
+        expect:
+        response.cookies[0].name == name
+        response.cookies[0].value == value
+        response.cookies[0].maxAge == maxAge
+        response.cookies[0].path == path
+        response.cookies[0].domain == domain
+        response.cookies[0].secure == secure
+        response.cookies[0].httpOnly == httpOnly
         response.cookies[0].version == 1
-    }
-
-    void "setCookie() with age"() {
-        when: service.setCookie('some_cookie_name', 'cookie_value', 42)
-        then:
-        response.cookies[0].name == 'some_cookie_name'
-        response.cookies[0].value == 'cookie_value'
-        response.cookies[0].maxAge == 42
-        response.cookies[0].version == 1
+        where:
+        args                                                                     | name          | value          | maxAge  | path    | domain         | secure | httpOnly
+        ['cookie_name', 'cookie_value']                                          | 'cookie_name' | 'cookie_value' | 2592000 | '/'     | null           | false  | false
+        ['cookie_name', 'cookie_value', 42]                                      | 'cookie_name' | 'cookie_value' | 42      | '/'     | null           | false  | false
+        ['cookie_name', 'cookie_value', 42, '/path']                             | 'cookie_name' | 'cookie_value' | 42      | '/path' | null           | false  | false
+        ['cookie_name', 'cookie_value', 42, '/path', '.example.com', true, true] | 'cookie_name' | 'cookie_value' | 42      | '/path' | '.example.com' | true   | true
     }
 
     def "deleteCookie() sets new cookie with same name but expired age"() {
-        when: service.deleteCookie('some_cookie_name') == null
+        when:
+        service.deleteCookie('some_cookie_name') == null
         then:
         response.cookies[0].name == 'some_cookie_name'
         response.cookies[0].value == null
