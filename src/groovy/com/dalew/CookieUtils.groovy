ae766531a4ba52fb0267194b4e6169401e40b61d
Sergey Ponomarev <stokito@gmail.com>
2014-11-21 17:43:39 +0200

    CookieUtils.groovy

--- src/groovy/com/dalew/CookieUtils.groovy
+++ src/groovy/com/dalew/CookieUtils.groovy
@@ -5,6 +5,10 @@ import grails.util.Holders
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+import static com.dalew.CookieUtils.COOKIE_DEFAULT_PATH
+import static com.dalew.CookieUtils.COOKIE_DEFAULT_HTTP_ONLY
+import static com.dalew.CookieUtils.COOKIE_DEFAULT_SECURE
+
 
 class CookieUtils {
     /** 30 days in seconds */
@@ -15,10 +19,13 @@ class CookieUtils {
     static final int COOKIE_AGE_TO_DELETE = 0
 
     static void extendReqResp() {
+        HttpServletRequest.metaClass.findCookie = { String name ->
+            return Holders.applicationContext.cookieService.findCookie(name)
+        }
         HttpServletRequest.metaClass.getCookie = { String name ->
             return Holders.applicationContext.cookieService.getCookie(name)
         }
-        HttpServletResponse.metaClass.setCookie = { String name, String value, Integer maxAge = null ->
+        HttpServletResponse.metaClass.setCookie = { String name, String value, Integer maxAge = null, String path = COOKIE_DEFAULT_PATH, String domain = null, boolean secure = COOKIE_DEFAULT_SECURE, boolean httpOnly = COOKIE_DEFAULT_HTTP_ONLY ->
             return Holders.applicationContext.cookieService.setCookie(name, value, maxAge)
         }
         HttpServletResponse.metaClass.deleteCookie = { String name, String domain = null ->
