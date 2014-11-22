787e937ad04e44a122584499bc5fbce890eb56ea
Sergey Ponomarev <stokito@gmail.com>
2014-11-22 06:45:26 +0200

    BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,7 @@ grails.project.dependency.resolution = {
 
     }
     plugins {
-        build(':release:3.0.1', ':rest-client-builder:2.0.1') {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
             export = false
         }
     }
