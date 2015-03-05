41add722f4b0461e7efbd05b3394f3577ac6a68e
Sergey Ponomarev <stokito@gmail.com>
2015-03-05 21:40:43 +0200

    v1.4 for Grails 2.4.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     plugins {
-        build(":release:2.2.0", ":rest-client-builder:1.0.3") {
+        build(":release:3.0.1", ":rest-client-builder:1.0.3") {
             export = false
         }
     }
