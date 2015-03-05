a3afe15015004cc23b28ab6f196e3e199941abb7
Sergey Ponomarev <stokito@gmail.com>
2015-03-06 00:36:16 +0200

    travis compile

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,15 +6,34 @@ grails.project.test.reports.dir = 'target/test-reports'
 
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    inherits('global')
-    log 'warn'
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
         grailsCentral()
-        mavenLocal()
-        mavenCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
     }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.21'
+    }
+
     plugins {
-        build(":release:2.2.0", ":rest-client-builder:1.0.3") {
+        build(":tomcat:$grailsVersion",
+                ":release:2.2.0",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
     }
