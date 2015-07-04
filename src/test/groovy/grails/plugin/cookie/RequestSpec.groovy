87350050043761e9ffd30240ab37f83192edfba1
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-07-03 20:56:02 -0500

    Use Groovy extension module

--- src/test/groovy/grails/plugin/cookie/RequestSpec.groovy
+++ src/test/groovy/grails/plugin/cookie/RequestSpec.groovy
@@ -4,9 +4,6 @@ import grails.test.mixin.TestFor
 
 @TestFor(CookieService)
 class RequestSpec extends CookieRequestSpec {
-    def setupSpec() {
-        CookieUtils.extendReqResp()
-    }
 
     @Override
     def setup() {
--- src/test/groovy/grails/plugin/cookie/ResponseSpec.groovy
+++ src/test/groovy/grails/plugin/cookie/ResponseSpec.groovy
@@ -4,9 +4,6 @@ import grails.test.mixin.TestFor
 
 @TestFor(CookieService)
 class ResponseSpec extends CookieResponseSpec {
-    def setupSpec() {
-        CookieUtils.extendReqResp()
-    }
 
     @Override
     def setup() {
