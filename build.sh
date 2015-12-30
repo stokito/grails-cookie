80eb631ea17f199d23b364c8f4026177c0ac872e
Alexey Zhokhov <alexey@zhokhov.com>
2015-12-31 02:47:41 +0800

    Reduced jar size.

--- build.sh
+++ build.sh
@@ -11,9 +11,15 @@ printf "\n# Checking for failed dependencies\n\n"
 ./gradlew dependencies | grep FAILED && exit 1
 
 if [ -n "${ARTIFACTORY_CONTEXT_URL}" ]; then
-    printf "\n# Assembling plugin\n\n"
-    ./gradlew assemble
+    printf "\n# Building jar\n\n"
+    ./gradlew jar
+
+    printf "\n# Building jar with sources\n\n"
+    ./gradlew sourcesJar
 
     printf "\n# Publishing plugin to Artifactory\n\n"
-    ./gradlew artifactoryPublish --info
+    ./gradlew artifactoryPublish
+else
+    printf "# Installing Grails plugin\n\n"
+    grails install
 fi
